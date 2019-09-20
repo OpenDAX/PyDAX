428771b3fb0c7d6a9407f5919743c964b25a9756
Phil Birkelbach <phil@petrasoft.net>
2019-09-19 20:53:46 -0500

    Work on get_cdt function and add more descriptive exceptions.

--- pydax/pydax.pyx
+++ pydax/pydax.pyx
@@ -25,8 +25,6 @@ import sys
 include "constants.pxi"
 cimport pydax
 
-cdef void cdt_callback(cdt_iter member, void* udata):
-    print(member.name)
 
 class Member():
     def __init__(self, client, name, type, count=1):
@@ -42,7 +40,6 @@ class Member():
     def __getitem__(self, index):
         self.index = index
         return self
-        print("index {}".format(index))
 
     def __setitem__(self, index, value):
         print("index {} = {}".format(index, value))
@@ -70,6 +67,7 @@ class Member():
 
     value = property(getValue, setValue)
 
+# The client class represents one connection to an OpenDAX Tag Server
 cdef class Client():
     cdef dax_state *__ds
     cdef char *name
@@ -81,8 +79,7 @@ cdef class Client():
         if self.__ds is NULL:
             raise MemoryError()
         x = dax_init_config(self.__ds, name)
-        if x != 0:  #TODO Better error description
-            raise RuntimeError()
+        if x != 0: raise getError(x)
         self.__tags = {}
 
     def __dealloc__(self):
@@ -112,9 +109,8 @@ cdef class Client():
                 m = Member(self, tag.name, tag.type, tag.count)
                 self.__tags[name] = m
                 return m
-            else:  #TODO Better error description
-                raise RuntimeError()
-
+            else:
+                raise getError(x)
 
     def add_cdt(self, name, members):
         cdef dax_cdt *cdt
@@ -125,38 +121,101 @@ cdef class Client():
         try:
             for m in members:
                 x = dax_cdt_member(self.__ds, cdt, m[0], m[1], m[2])
-                if x != 0:
-                    raise RuntimeError
+                if x != 0: raise getError(x)
             x = dax_cdt_create(self.__ds, cdt, &t)
-            if x != 0:
-                raise RuntimeError
+            if x != 0: raise getError(x)
         finally:
             dax_cdt_free(cdt)
 
-
     def get_cdt(self, name):
         cdef tag_type t
+        tlist = []
         t = dax_string_to_type(self.__ds, name)
 
-        x = dax_cdt_iter(self.__ds, t, NULL, cdt_callback)
+        x = dax_cdt_iter(self.__ds, t, <void*>tlist, cdt_callback)
+        return tlist
 
     # Direct wrappers for libdax library functions
     def dax_configure(self):
         x = dax_configure(self.__ds, 1, [self.name], 0)
-        if x != 0:  #TODO Better error description
-            raise RuntimeError()
+        if x != 0: raise getError(x)
 
     def dax_connect(self):
         x = dax_connect(self.__ds)
-        if x != 0:  #TODO Better error description
-            raise RuntimeError()
+        if x != 0: raise getError(x)
 
     def dax_tag_add(self, name, type, size):
         cdef Handle h
         x = dax_tag_add(self.__ds, &h, name, type, size)
-        if x != 0:  #TODO Better error description
-            raise RuntimeError()
+        if x != 0: raise getError(x)
 
         # self.__tags[name] = Member(self, name, h)
         # print(self.__tags)
         # return self.__tags[name]
+
+# This is the callback function for getting the compound data type from
+# the OpenDAX server.  The client calls the dax_cdt_iter() function with
+# this function as the callback.  It is expected that udata is a Python
+# list and this funtion will append the CDT member represented by the
+# call to this function to that list as a Tuple.
+cdef void cdt_callback(cdt_iter member, void* udata):
+    l = (<object>udata)
+    l.append((member.name, member.type, member.count))
+
+# This function returns an exception object that depends on the value of the
+# error 'e'
+def getError(e):
+    print(e)
+    exceptions = {
+                    ERR_GENERIC:RuntimeError,
+                    ERR_NO_SOCKET:RuntimeError,
+                    ERR_2BIG:RuntimeError,
+                    ERR_ARG:AttributeError,
+                    ERR_NOTFOUND:AttributeError,
+                    ERR_MSG_SEND:RuntimeError,
+                    ERR_MSG_RECV:RuntimeError,
+                    ERR_TAG_BAD:RuntimeError,
+                    ERR_TAG_DUPL:RuntimeError,
+                    ERR_ALLOC:MemoryError,
+                    ERR_MSG_BAD:RuntimeError,
+                    ERR_DUPL:RuntimeError,
+                    ERR_NO_INIT:RuntimeError,
+                    ERR_TIMEOUT:RuntimeError,
+                    ERR_ILLEGAL:RuntimeError,
+                    ERR_INUSE:RuntimeError,
+                    ERR_PARSE:RuntimeError,
+                    ERR_ARBITRARY:RuntimeError,
+                    ERR_NOTNUMBER:RuntimeError,
+                    ERR_EMPTY:RuntimeError,
+                    ERR_BADTYPE:TypeError,
+                    ERR_AUTH:RuntimeError
+                }
+    descriptions = {
+                    ERR_GENERIC:"",
+                    ERR_NO_SOCKET:"No Socket Found",
+                    ERR_2BIG:"Argument Too Big",
+                    ERR_ARG:"Argument",
+                    ERR_NOTFOUND:"Not Found",
+                    ERR_MSG_SEND:"Message Send Failure",
+                    ERR_MSG_RECV:"Message Receive Failure",
+                    ERR_TAG_BAD:"Bad Tag",
+                    ERR_TAG_DUPL:"Duplicate Tag",
+                    ERR_ALLOC:"Allocation Error",
+                    ERR_MSG_BAD:"Bad Argument",
+                    ERR_DUPL:"Duplicate",
+                    ERR_NO_INIT:"Not Initialized",
+                    ERR_TIMEOUT:"Timeout",
+                    ERR_ILLEGAL:"Illegal",
+                    ERR_INUSE:"In Use",
+                    ERR_PARSE:"Parsing Error",
+                    ERR_ARBITRARY:"Arbitrary",
+                    ERR_NOTNUMBER:"Not a Number",
+                    ERR_EMPTY:"Empty",
+                    ERR_BADTYPE:"Bad Type",
+                    ERR_AUTH:"Not Authorized"
+                   }
+
+    if e in exceptions.keys():
+        return exceptions[e](descriptions[e])
+    else:
+        return RuntimeError("Unknown Error")
